4424f9dc9088f8280859d00477a05aa957423b98
MateuszBachor <mateuszbachor3@gmail.com>
2022-09-20 15:17:18 +0200

    app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
 
 import AddContactForm from '../AddContactForm/AddContactForm';
 import FindContact from 'components/FindContact/FindContact';
@@ -8,43 +6,11 @@ import ContactList from 'components/ContactList/ContactList';
 import styles from './App.module.css';
 
 export const App = () => {
-  // const handleSubmit = evt => {
-  //   evt.preventDefault();
-  //   const form = evt.currentTarget;
-  //   const contact = {
-  //     name: form.elements.contact.value,
-  //     number: form.elements.number.value,
-  //     id: nanoid(),
-  //   };
-
-  //   if (
-  //     contacts.filter(
-  //       el => el.name.toLocaleLowerCase() === contact.name.toLocaleLowerCase()
-  //     ).length >= 1
-  //   ) {
-  //     alert(`${contact.name} is already in contacts`);
-  //     return;
-  //   }
-  //   setContacts(() => [...contacts, contact]);
-  //   form.reset();
-  // };
-
-  // const removeContact = id => {
-  //   setContacts(() => contacts.filter(contact => contact.id !== id));
-  // };
-  // const find = event => {
-  //   setFilter(() => event.target.value);
-  // };
-
   return (
     <div className={styles.whatever}>
       <AddContactForm />
       <FindContact />
-      <ContactList
-      // filterValue={filter}
-      // contactsArray={contacts}
-      // remove={removeContact}
-      />
+      <ContactList />
     </div>
   );
 };
